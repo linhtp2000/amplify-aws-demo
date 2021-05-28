5af932c74c8b0dc3e76705c88fd8b9987c7d48f4
linhtp2000 <linhtp157@gmail.com>
2021-05-29 00:36:22 +0700

    update2

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
       <header className="App-header">
         <AmplifySignOut />
         <h2>Cloud computing - AWS Amplify for web - Nhóm 15</h2>
+        <h2>Amplify-demo-update2</h2>
         <h2>Tô Thị Mỹ Nhân</h2>
         <h2>Trần Phương Linh</h2>
       </header>
