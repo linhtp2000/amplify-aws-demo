31e4b37e4a6ecb586cbb0f65e5cc36a92bb0ac80
linhtp2000 <linhtp157@gmail.com>
2021-05-28 23:24:04 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AmplifySignOut />
+        <h2>Cloud computing - AWS Amplify for web - Nhóm 15</h2>
+        <h2>Tô Thị Mỹ Nhân</h2>
+        <h2>Trần Phương Linh</h2>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
